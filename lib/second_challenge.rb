109b4103c8d969b282657bb214def492e987437b
Cloe Kouadjo <armelfli50@gmail.com>
2019-08-13 23:55:57 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -9,7 +9,7 @@ def second_challenge
     :capulet => {
           :patriarch =>{},
           :matriarch =>{},
-          :heroinen =>{},
+          :heroine =>{},
           :heroine_friends =>{}
       }}
 
