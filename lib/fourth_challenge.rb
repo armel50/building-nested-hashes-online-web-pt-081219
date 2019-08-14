2829e4a289611806c7a1798ef8237b962cf47fb9
Cloe Kouadjo <armelfli50@gmail.com>
2019-08-14 00:04:35 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -11,6 +11,11 @@ def fourth_challenge
           :age => "17",
           :attitude => "worried"
         },
+        {
+          :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"
+        },
       ]
    }, 
    :capulet => {
